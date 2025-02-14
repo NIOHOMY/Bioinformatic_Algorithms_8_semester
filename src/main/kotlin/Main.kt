81f52c90ca341d3d6462430b1d1b8a141114d6ef
Polina Pahomova <pahomova@surf.dev>
2025-02-15 00:41:56 +0300

    lab1 - добавлены тестовые примеры алгоритмов

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,54 @@
 package labs.com
 
+import labs.com.algorithms.lab1.*
+
 fun main() {
-    println("Hello World!")
+    println()
+    testRabbitPopulation()
+    println()
+    testDnaSequenceAnalyzer()
+    println()
+    testHammingDistanceCalculator()
+    println()
+    testRnaSequenceGenerator()
+    println()
+    testProteinSequenceGenerator()
+}
+
+fun testRabbitPopulation() {
+    val rabbitPopulation = RabbitPopulation()
+    val n = 5
+    val k = 3
+
+    val totalPairs = rabbitPopulation.calculatePopulation(n.toLong(), k.toLong())
+    println("months: $n total rabbits pairs: $totalPairs")
+}
+
+fun testDnaSequenceAnalyzer() {
+    val dnaSequenceAnalyzer = DnaSequenceAnalyzer()
+    val dnaSequence = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"
+    val nucleotideCounts = dnaSequenceAnalyzer.countNucleotides(dnaSequence)
+    println("Nucleotide counts: $nucleotideCounts")
+}
+
+fun testHammingDistanceCalculator() {
+    val hammingDistanceCalculator = HammingDistanceCalculator()
+    val s = "GAGCCTACTAACGGGAT"
+    val t = "CATCGTAATGACGGCCT"
+    val distance = hammingDistanceCalculator.calculateHammingDistance(s, t)
+    println("Hamming distance: $distance")
+}
+
+fun testRnaSequenceGenerator() {
+    val rnaSequenceGenerator = RnaSequenceGenerator()
+    val dnaSequence = "GATGGAACTTGACTACGTAAATT"
+    val rnaSequence = rnaSequenceGenerator.generateRnaSequence(dnaSequence)
+    println("RNA sequence: $rnaSequence")
+}
+
+fun testProteinSequenceGenerator() {
+    val proteinSequenceGenerator = ProteinSequenceGenerator()
+    val rnaSequence = "AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA"
+    val proteinSequence = proteinSequenceGenerator.generateProteinSequence(rnaSequence)
+    println("Protein sequence: $proteinSequence")
 }
