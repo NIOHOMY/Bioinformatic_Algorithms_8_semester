7808475874da0d342cdcad61ce301fcac48c7aee
Polina Pahomova <pahomova@surf.dev>
2025-03-05 20:54:50 +0300

    lab2 - добавлен считыватель данных из файла

---
+++ src/main/kotlin/utils/FileReader.kt
@@ -0,0 +1,15 @@
+package labs.com.utils
+
+import java.io.File
+
+class FileReader {
+
+    fun readFromFile(filePath: String, linesToDrop: Int = 0): String {
+        val file = File(filePath)
+        if (!file.exists()) {
+            throw IllegalArgumentException("File does not exist")
+        }
+
+        return file.readLines().drop(linesToDrop).joinToString("\n")
+    }
+}
