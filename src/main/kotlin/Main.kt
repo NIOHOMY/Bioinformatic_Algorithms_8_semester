8f2157d22a8e8bf516b991565640390e3fd1870f
Polina Pahomova <pahomova@surf.dev>
2025-03-15 17:37:15 +0300

    lab4 - добавлены базовые ресурсы и утилиты

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,26 @@
 package labs.com
 
+import labs.com.utils.FileReader
+
 fun main() {
-    println("Hello World!")
+    println()
+    task1()
+    println()
+}
+
+private fun task1() {
+
+    val fileReader = FileReader()
+    try {
+        val str1 = fileReader.readFromFile(
+            "C:/Users/N1o/IdeaProjects/Bioinformatic_Algorithms_8_semester/src/main/resources/human_epidermal_growth_factor.txt",
+            1
+        )
+        val str2 = fileReader.readFromFile(
+            "C:/Users/N1o/IdeaProjects/Bioinformatic_Algorithms_8_semester/src/main/resources/pig_epidermal_growth_factor.txt",
+            1
+        )
+    } catch (e: Exception) {
+        println("Error: ${e.message}")
+    }
 }
